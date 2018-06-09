f7feb019b8ea464802dd72014ea005b59eb810e6
Antonio Ivanovski <Antonio.Ivanovski@netcetera.com>
2018-06-09 13:21:00 +0200

    Define base annotations

---
+++ aura-annotation/src/main/java/netcetera/com/aura/AuraInteractor.java
@@ -0,0 +1,12 @@
+package netcetera.com.aura;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+public @interface AuraInteractor {
+
+}
